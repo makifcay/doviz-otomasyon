851269584ffb40cc1e5421009e6bddce3b521bf1
makifcay <m.akif-46@outlook.com>
2020-04-10 04:39:31 +0300

    Top menu eklendi

--- src/Program.cs
+++ src/Program.cs
@@ -16,7 +16,7 @@ namespace DovizOtomasyon
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(true);
-            Application.Run(new Form1());
+            Application.Run(new FormMain());
         }
     }
 }
