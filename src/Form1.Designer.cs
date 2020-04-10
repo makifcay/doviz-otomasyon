abbcea53dbcd59bad68dfb5e7fcf062842e24174
makifcay <m.akif-46@outlook.com>
2020-04-10 03:02:08 +0300

    FontAwesome eklendi, .drawio düzenlendi

--- src/Form1.Designer.cs
+++ src/Form1.Designer.cs
@@ -28,25 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.metroButton1 = new MetroFramework.Controls.MetroButton();
             this.SuspendLayout();
-            // 
-            // metroButton1
-            // 
-            this.metroButton1.Location = new System.Drawing.Point(388, 203);
-            this.metroButton1.Name = "metroButton1";
-            this.metroButton1.Size = new System.Drawing.Size(75, 23);
-            this.metroButton1.TabIndex = 0;
-            this.metroButton1.Text = "metroButton1";
-            this.metroButton1.UseSelectable = true;
-            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(882, 853);
-            this.Controls.Add(this.metroButton1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -54,8 +42,6 @@
         }
 
         #endregion
-
-        private MetroFramework.Controls.MetroButton metroButton1;
     }
 }
 
